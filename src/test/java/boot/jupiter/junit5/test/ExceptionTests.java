b89e237aae6936cc9e038689a99854012433fdaf
DESKTOP-OSRLKA2\tkddj <tkddjs0116@gmail.com>
2020-02-15 11:28:30 +0900

    Add ExceptionTests (assertThrows)

---
+++ src/test/java/boot/jupiter/junit5/test/ExceptionTests.java
@@ -0,0 +1,29 @@
+package boot.jupiter.junit5.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+@SpringBootTest
+public class ExceptionTests {
+
+    @Test
+    void shouldThrowException() {
+        Throwable exception = assertThrows(UnsupportedOperationException.class, () -> {
+            throw new UnsupportedOperationException("Not supported");
+        });
+        assertEquals(exception.getMessage(), "Not supported");
+    }
+
+    @Test
+    void assertThrowsException() {
+        String str = null;
+        assertThrows(IllegalArgumentException.class, () -> {
+           Integer.valueOf(str);
+        });
+    }
+
+}
