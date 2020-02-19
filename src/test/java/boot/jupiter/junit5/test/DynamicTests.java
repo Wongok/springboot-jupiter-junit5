cadae97cc5b21546aa423b0c1caa995110aeaee0
DESKTOP-OSRLKA2\tkddj <tkddjs0116@gmail.com>
2020-02-19 20:11:19 +0900

    Add DynamicTests (return Iterable)

---
+++ src/test/java/boot/jupiter/junit5/test/DynamicTests.java
@@ -0,0 +1,22 @@
+package boot.jupiter.junit5.test;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class DynamicTests {
+
+    @TestFactory
+    Iterable<DynamicTest> dynamicTestsWithIterable() {
+        return Arrays.asList(
+                DynamicTest.dynamicTest("Add test",
+                        () -> assertEquals(2, Math.addExact(1, 1))),
+                DynamicTest.dynamicTest("Multiply Test",
+                        () -> assertEquals(4, Math.multiplyExact(2, 2))));
+    }
+}
