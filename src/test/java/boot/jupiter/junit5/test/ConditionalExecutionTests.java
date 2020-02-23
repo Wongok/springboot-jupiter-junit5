e2876ecdc53a0840771d237766b09e2b75d6f922
DESKTOP-OSRLKA2\tkddj <tkddjs0116@gmail.com>
2020-02-23 15:00:15 +0900

    Add ConditionalExecutionTests (JRE Version Example)

--- src/test/java/boot/jupiter/junit5/test/ConditionalExecutionTests.java
+++ src/test/java/boot/jupiter/junit5/test/ConditionalExecutionTests.java
@@ -1,12 +1,14 @@
 package boot.jupiter.junit5.test;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnJre;
 import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledOnJre;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.condition.OS.*;
-
+import static org.junit.jupiter.api.condition.JRE.*;
 @SpringBootTest
 public class ConditionalExecutionTests {
 
@@ -27,5 +29,24 @@ public class ConditionalExecutionTests {
     @DisabledOnOs(WINDOWS)
     void not_Windows_OS() {
         System.out.println("not Windows");
+    }
+
+    // Jre
+    @Test
+    @EnabledOnJre(JAVA_8)
+    void java8() {
+        System.out.println("java8");
+    }
+
+    @Test
+    @EnabledOnJre({JAVA_9, JAVA_10})
+    void java9or10() {
+        System.out.println("java 9 or 10");
+    }
+
+    @Test
+    @DisabledOnJre(JAVA_11)
+    void not_java11() {
+        System.out.println("not Java 11");
     }
 }
